366235e43d67dc1ae1847832d625ffa5044cbbbd
kunal_manchanda <kunalmanchanda189@gmail.com>
2023-12-31 14:23:31 +0530

    attendance report updated

--- src/components/Reports/AttendanceReport/AttendanceReport.tsx
+++ src/components/Reports/AttendanceReport/AttendanceReport.tsx
@@ -1,6 +1,6 @@
-import { MenuItem, TextField } from '@mui/material'
-import React from 'react'
-
+import { Box, Button, Grid, MenuItem, TextField, Typography } from "@mui/material";
+import React from "react";
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 interface Machine {
   id: number;
   title: string;
@@ -14,19 +14,25 @@ interface Machine {
   humidity: number;
 }
 
-interface AttendanceReportsprops{
-
-  Machines:Machine[]
+interface AttendanceReportsprops {
+  Machines: Machine[];
 }
-const AttendanceReport:React.FC<AttendanceReportsprops> = ({Machines}) => {
+
+const duration = [
+  { id: 1, label: "Last 24 hrs", value: "24hrs" },
+  { id: 2, label: "Last 2 hrs", value: "2hrs" },
+  { id: 3, label: "Last 7 days", value: "7days" },
+];
+const AttendanceReport: React.FC<AttendanceReportsprops> = ({ Machines }) => {
   return (
-    <div>
-       <TextField
-        
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={4} md={4} lg={4} xl={3}>
+        <TextField
           select
           label="Machine"
           defaultValue={Machines[0].title}
           sx={{}}
+          fullWidth
         >
           {Machines.map((option) => (
             <MenuItem key={option.id} value={option.title}>
@@ -34,8 +40,53 @@ const AttendanceReport:React.FC<AttendanceReportsprops> = ({Machines}) => {
             </MenuItem>
           ))}
         </TextField>
-    </div>
-  )
-}
+      </Grid>
+      <Grid item xs={12} sm={4} md={4} lg={4} xl={3}>
+        <TextField
+          select
+          label="Duration"
+          defaultValue={duration[0].label}
+          sx={{ width: "100%" }}
+          fullWidth
+        >
+          {duration.map((option) => (
+            <MenuItem key={option.id} value={option.label}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sm={2}
+        md={2}
+        lg={2}
+        xl={2}
+        sx={{ padding: 2, marginY: 1 }}
+        className=""
+      >
+        <Button variant="contained" fullWidth sx={{padding:1}}>
+          FETCH
+        </Button>
+      </Grid>
+      <Grid item xs={12} sm={6} md={6} lg={8} xl={8}>
+        <Typography variant="h6" color="text.secondary">
+          Allowed : <span className="font-semibold text-black">24/30</span>
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6} md={6} lg={4} xl={4} className="p-2 text-right">
+      <Button variant="outlined" sx={{}}><FileDownloadOutlinedIcon/>Download</Button>
+      </Grid>
 
-export default AttendanceReport
+      <Grid item xs={12} sm={6} md={6} lg={4} xl={12}>
+
+            <Box className="outline-dashed	 h-72"></Box>
+
+      </Grid>
+
+    </Grid>
+  );
+};
+
+export default AttendanceReport;
